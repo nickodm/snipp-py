bfb9eb2a2ce8286f0a678fa966e55287476e3f0e
Nicko <nmcolivoro@outlook.cl>
2026-05-18 18:31:53 -0400

    Implemented XDG standard

--- src/snipp/core/paths.py
+++ src/snipp/core/paths.py
@@ -1,18 +1,21 @@
 from pathlib import Path
+from platformdirs import PlatformDirs
 
-__all__ = ["BASE_DIR", "SNIPPETS"]
+__all__ = ["SNIPPETS", "TEMP"]
 
-BASE_DIR: Path = Path("~/.snipp").expanduser()
-"""The base program's directory."""
+dirs = PlatformDirs("snipp", "NickoDM", ensure_exists=True, roaming=True)
 
-SNIPPETS: Path = BASE_DIR / "snippets"
+SNIPPETS: Path = dirs.user_data_path / "snippets"
 """The directory where the snippets are saved."""
+
+TEMP: Path = dirs.user_runtime_path
+"""The directory where the temporary files are saved."""
 
 def init_project_dir() -> None:
     """Create all the project directories that should exist for the
     correct execution of the program.
     """
-    paths = [BASE_DIR, SNIPPETS]
+    paths = [SNIPPETS]
     
     for path in paths:
         if not path.exists():
