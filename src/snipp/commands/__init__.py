01f8a63b5fef2ae787548f907861aa876b1642fb
Nicko <nmcolivoro@outlook.cl>
2026-05-18 01:29:16 -0400

    Added purge command

--- src/snipp/commands/__init__.py
+++ src/snipp/commands/__init__.py
@@ -1,5 +1,8 @@
-
+"""
+Module containing all the commands of the program.
+"""
 __all__ = ["create", "list", "show", "delete", "deploy", "export", "rename",
-           "import_snipp"]
+           "import_snipp", "purge"]
 
-from . import (create, list, show, delete, deploy, export, import_snipp, rename)
+from . import (create, list, purge, show, delete, deploy, export, import_snipp, 
+    rename)
---
+++ src/snipp/commands/purge.py
@@ -0,0 +1,51 @@
+from rich.prompt import Confirm
+import os
+
+from ..core import *
+from ..core.parser import *
+
+def remove(snippet: Snippet) -> None:
+    try:
+        os.remove(snippet.path)
+    except PermissionError:
+        printerr(f":cross_mark: Cannot remove \"{snippet}\": "
+                 "The file is being used or the process has no permission to "
+                 "delete it.")
+
+def main(force: bool) -> int:
+    counter = 0
+    for snippet in load_snippets():
+        name = snippet.name
+        id = snippet.uuid[:ID_MIN_LEN]
+
+        if force:
+            remove(snippet)
+            print(f"Deleted snippet \"[blue]{name}[/]\" ([yellow]{id}[/]).")
+            counter += 1
+            continue
+        
+        answer: bool = Confirm.ask(f"Delete snippet \"[blue]{name}[/]\" "
+                             f"([yellow]{id}[/])?")
+        
+        if answer:
+            remove(snippet)
+            print(":white_check_mark: [red]Deleted.")
+            counter += 1
+        else:
+            print(":cross_mark: [green]Not deleted.")
+    
+    print(f"Removed {counter} snippets.")
+    return 0
+
+@command_register
+def register(cmds: SubParser) -> None:
+    cmd = cmds.add_parser(
+        name="purge",
+        help="delete all the saved snippets",
+        description="Delete all the saved snippets."
+    )
+    
+    cmd.add_argument("-f", "--force", action="store_true",
+        help="don't ask for confirmation for each snippet")
+    
+    cmd.set_defaults(func=main)
